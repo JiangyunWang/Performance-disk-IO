887ef2b795d566adff1072be7610adfe56f035a7
Jiangyun Wang <jurainyun@gmail.com>
2021-12-01 13:40:20 -0500

    io

--- io.c
+++ io.c
@@ -11,81 +11,103 @@
 #include <sys/stat.h>
 #include <time.h>
 
-
-
-void errorMessage(char *s){
+void errorMessage(char *s)
+{
     perror(s);
     exit(1);
 }
 
-int main(int argc, char* argv[])
+unsigned int xorbuf(int *buffer, int size)
+{
+    unsigned int result = 0;
+    for (int i = 0; i < size; i++)
+    {
+        result ^= buffer[i];
+    }
+    return result;
+}
+
+int main(int argc, char *argv[])
 {
     //  ./run <filename> [-r|-w] <block_size> <block_count>
 
-    if (argc!=5) {
+    if (argc != 5)
+    {
         errorMessage("Invalid argument amount.\n It should be: ./run <filename> [-r|-w] <block_size> <block_count>");
     }
 
     ssize_t block_size = atoi(argv[3]);
     ssize_t block_count = atoi(argv[4]);
-    if (block_size<=0 || block_count<=0) errorMessage("Invalid block amount");
+    if (block_size <= 0 || block_count <= 0)
+        errorMessage("Invalid block amount");
 
-    // ???  malloc  or  char[]
-    char* buf = malloc(block_size*block_count*sizeof(char));
-    size_t result;
-    float amount = 0;
+    ssize_t result;
+    float amount = block_size * block_count;
+    int *buf = malloc(amount * sizeof(int));
 
-    FILE *fd;
-    printf("block_size = %zd and block_count= %zd\n ", block_size, block_count);
-    if (argv[2][0]=='-' && argv[2][1]=='r') { 
-         fd =fopen(argv[1], "r");
-        if (fd) {
+    int fd;
+    // printf("block_size = %zd and block_count= %zd\n ", block_size, block_count);
+    if (argv[2][0] == '-' && argv[2][1] == 'r')
+    {
+        fd = open(argv[1], O_RDONLY);
+        if (fd)
+        {
             // use clock for precison clock
             clock_t begin = clock();
             printf("Start clock = %s", ctime(&begin));
-            while (!feof(fd)) {
-            result = fread(buf, block_size, block_count, fd);
-            amount+=result;
+            result = read(fd, buf, amount);
             // printf("result: %zu\n", result);
-            }
-            
+
             clock_t finish = clock();
             double time_spent = (double)(finish - begin) / CLOCKS_PER_SEC;
             printf("End clock = %s", ctime(&finish));
-            printf("B/s: %f\n", amount/time_spent);
-            amount/=1000000;
-            
+            printf("B/s: %f\n", amount / time_spent);
+            printf("xorbuf: %08x\n", xorbuf(buf, amount));
+            amount /= 1000000;
 
-            printf("Mib/s: %f\n", amount/time_spent);
+            printf("Mib/s: %f\n", amount / time_spent);
             free(buf);
-
-        } else {
+            close(fd);
+        }
+        else
+        {
             errorMessage("Invalid file");
         }
-    } 
-    // else if (argv[2][0]=='-'&&argv[2][1]=='w') {
-    //     fd =fopen(argv[1], "w");
-    //     if (fd) {
-    //         time(&start);
-    //         printf("Start time = %s", ctime(&start));
-    //         while (!feof(fd)) {
-    //         result = fwrite(buf, block_size, block_count, fd);
-    //         // printf("result: %zu\n", result);
-    //         }
+    }
+    else if (argv[2][0] == '-' && argv[2][1] == 'w')
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            buf[i] = 'a';
+        }
+        fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (fd)
+        {
+            // use clock for precison clock
+            clock_t begin = clock();
+            printf("Start clock = %s", ctime(&begin));
+            result = write(fd, buf, amount);
+            // printf("result: %zu\n", result);
 
-    //         time(&end);
-    //         printf("End time = %s", ctime(&end));
-    //         free(buf);
+            clock_t finish = clock();
+            double time_spent = (double)(finish - begin) / CLOCKS_PER_SEC;
+            printf("End clock = %s", ctime(&finish));
+            printf("B/s: %f\n", amount / time_spent);
+            amount /= 1000000;
 
-    //     } else {
-    //         errorMessage("Invalid file");
-    //     }
+            printf("Mib/s: %f\n", amount / time_spent);
+            free(buf);
+            close(fd);
+        }
+        else
+        {
+            errorMessage("Invalid file");
+        }
+    }
+    else
+    {
+        errorMessage("Invalid input");
+    }
 
-    // } else  {
-    //     errorMessage("Invalid input");
-    // }
-
-
-   return 0;
+    return 0;
 }
-
