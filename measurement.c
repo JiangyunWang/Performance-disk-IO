98455f1627c5c80a95d4313b09876bf14d760041
Jiangyun Wang <jurainyun@gmail.com>
2021-12-04 18:13:57 -0500

    fix xor

--- measurement.c
+++ measurement.c
@@ -1,4 +1,4 @@
-#define _FILE_OFFSET_BITS 64
+
 #include <stddef.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,15 +10,15 @@
 #include <sys/stat.h>
 #include <time.h>
 // #include "io.c"
-
+unsigned int xorResult = 0;
 unsigned int xorbuf(int *buffer, int size)
 {
-    unsigned int result = 0;
+
     for (int i = 0; i < size; i++)
     {
-        result ^= buffer[i];
+        xorResult ^= buffer[i];
     }
-    return result;
+    return xorResult;
 }
 
 void writeFile(char *file, ssize_t block_size)
@@ -28,27 +28,27 @@ void writeFile(char *file, ssize_t block_size)
     {
 
         double amount = 0;
-        clock_t begin = clock();
+
         double result = 0;
-        int cnt = 0;
-        int *buf = malloc(block_size * sizeof(int));
-        printf("Start clock = %ld\n", begin);
+        int *buf = malloc(block_size);
+        clock_t begin = clock();
         while ((result = read(fd, buf, block_size)) > 0)
         {
             amount += result;
-            realloc(buf, block_size * sizeof(int));
-            cnt++;
+            xorbuf(buf, block_size);
         }
 
         // printf("amount: %d\n", amount);
 
         clock_t finish = clock();
         double time_spent = (double)(finish - begin) / CLOCKS_PER_SEC;
-        printf("time spend: %f\n", time_spent);
-        printf("The file size is: %f\n", amount);
+
+        printf("Start clock = %ld\n", begin);
         printf("End clock = %ld\n", finish);
+        printf("time spend: %f\n\n", time_spent);
+        printf("The file size is: %f\n", amount);
         printf("B/s: %f\n", amount / time_spent);
-        printf("xorbuf: %08x\n", xorbuf(buf, amount));
+        printf("xorbuf: %08x\n", xorResult);
         amount /= 1048576;
 
         printf("Mib/s: %f\n", amount / time_spent);
